9c3b1f318b19fa0db1f4a6ef5bb53ba54d4f8037
kasule joseph <joseph.kasule@andela.com>
2019-11-06 10:12:59 +0200

    add form fields and labels

--- service-provider/src/components/IspModal.jsx
+++ service-provider/src/components/IspModal.jsx
@@ -25,16 +25,68 @@ export class Modal extends Component {
                 >
                     <div className="modal-content black-text left-align">
                         <h4>New ISP Provider</h4>
-                        gxfchvjbkhcfgxchvjb 
-                        <p>A bunch of text</p>
+                        <div className="row">
+                            <form className="col s12">
+                                <div className="row">
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">account_circle</i>
+                                        <input id="icon_prefix" type="text" className="validate" />
+                                        <label htmlFor="icon_prefix">First Name</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">phone</i>
+                                        <input id="icon_telephone" type="tel" className="validate" />
+                                        <label htmlFor="icon_telephone">Telephone</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">email</i>
+                                        <input id="icon_prefix" type="text" className="validate" />
+                                        <label htmlFor="icon_prefix">Email</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">link</i>
+                                        <input id="icon_telephone" type="tel" className="validate" />
+                                        <label htmlFor="icon_telephone">Url</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">attach_money</i>
+                                        <input id="icon_prefix" type="text" className="validate" />
+                                        <label htmlFor="icon_prefix">Lowest Price</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">wifi</i>
+                                        <input id="icon_telephone" type="tel" className="validate" />
+                                        <label htmlFor="icon_telephone">Max Speed</label>
+                                    </div>
+                                    <div className="input-field col s6">
+                                        <i className="material-icons prefix">mode_edit</i>
+                                        <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
+                                        <label htmlFor="icon_prefix2">Description</label>
+                                    </div>
+                                    <div className="file-field input-field">
+                                        <div className="btn">
+                                            <span>Image</span>
+                                            <input type="file" />
+                                        </div>
+                                        <div className="file-path-wrapper">
+                                            <input className="file-path validate" type="text" placeholder="Upload an image" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <button className="btn waves-effect waves-light" type="submit" name="action">
+                                    Submit
+                                        <i className="material-icons right">send</i>
+                                </button>
+                            </form>
+                        </div>
                     </div>
                     <div className="modal-footer">
                         <a href="#!" className="modal-close waves-effect waves-red btn-flat">
                             Disagree
-            </a>
+                        </a>
                         <a href="#!" className="modal-close waves-effect waves-green btn-flat">
                             Agree
-            </a>
+                        </a>
                     </div>
                 </div>
             </div>
