a2b1c9e3bf4893af82bb0a9811611103e1efb2bd
kasule joseph <joseph.kasule@andela.com>
2019-11-11 16:29:08 +0200

    deploy app on netlify

---
+++ build/precache-manifest.cca1b5df48f4395f863a41ad442b62c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad348b09c1122dde5b13611e3b9a325f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0f6757fe308842fc4f2",
+    "url": "./static/css/main.134e53b0.chunk.css"
+  },
+  {
+    "revision": "f1ea29d02e13e2ee4f94",
+    "url": "./static/js/2.f96d7330.chunk.js"
+  },
+  {
+    "revision": "e0f6757fe308842fc4f2",
+    "url": "./static/js/main.5d59929a.chunk.js"
+  },
+  {
+    "revision": "102e62a27a89cb810743",
+    "url": "./static/js/runtime-main.4fc54a4a.js"
+  }
+]);
--- src/components/ISPModal.jsx
+++ src/components/ISPModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 
 const ISPModal = ({
   thisModal, state, handleChange, handleSubmit,
